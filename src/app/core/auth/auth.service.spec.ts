848fe1d490b6d8bcf5349fc53ac176d2734b79bb
vbianchidev <vitor.biansil@gmail.com>
2022-08-24 21:59:09 -0300

    [feat(core/auth)] create auth service and guard

---
+++ src/app/core/auth/auth.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should be authenticated', () => {
+    expect(service.isAuthenticated()).toEqual(true);
+  });
+});
