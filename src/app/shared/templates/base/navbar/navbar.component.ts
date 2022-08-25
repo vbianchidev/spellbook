efa409159fd9f15929d4a1861394a7b140e325e6
vbianchidev <vitor.biansil@gmail.com>
2022-08-24 21:56:32 -0300

    [feat(base)] create navbar component

---
+++ src/app/shared/templates/base/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
