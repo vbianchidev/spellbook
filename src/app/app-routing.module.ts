3849abfe77a7ce1a981dd545f0bdb57062ac23c7
vbianchidev <vitor.biansil@gmail.com>
2022-08-24 22:21:25 -0300

    [style] remove blank spaces from app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }, 
-  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) }
+  { 
+    path: 'dashboard', 
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) 
+  }, { 
+    path: 'login', 
+    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) 
+  }
 ];
 
 @NgModule({
