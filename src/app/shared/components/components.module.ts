2abc89726dc1e2dbe291a9c3f6226ab8385efd36
Vitor Bianchi <vitor.biansil@gmail.com>
2022-10-17 17:37:32 -0300

    [refact] add table module

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatTableModule } from '@angular/material/table';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { PipesModule } from '../pipes/pipes.module';
 import { CardComponent } from './card/card.component';
 import { IconComponent } from './icon/icon.component';
-import { SpellLevelPipe } from '../pipes/spell-level.pipe';
-import { PipesModule } from '../pipes/pipes.module';
 
 
 
@@ -17,6 +18,7 @@ import { PipesModule } from '../pipes/pipes.module';
     FontAwesomeModule, 
     MatCardModule, 
     MatChipsModule,
+    MatTableModule,
   ],
   declarations: [CardComponent, IconComponent],
   exports:      [CardComponent, IconComponent],
