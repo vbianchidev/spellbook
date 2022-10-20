dd56ccc132adad384dd744deb232e1990ab3c0c8
Vitor Bianchi <vitor.biansil@gmail.com>
2022-10-20 20:08:27 -0300

    [feat]  create card module

---
+++ src/app/shared/components/card/card.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { PipesModule } from '../../pipes/pipes.module';
+import { CardComponent } from './card.component';
+
+
+
+@NgModule({
+  imports: [
+    PipesModule,
+    CommonModule, 
+    FontAwesomeModule, 
+    MatIconModule,
+    MatListModule,
+    MatCardModule, 
+    MatChipsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    CardModule
+  ],
+  declarations: [CardComponent],
+  exports:      [CardComponent],
+})
+export class CardModule { }
